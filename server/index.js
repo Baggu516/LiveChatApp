4ec8bd27f2582ba1f99e4fd2e31ac10d4c38080b
Baggu516 <bhargavsainallagutla@gmail.com>
2023-12-12 11:15:00 +0530

    first coomit

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express=require("express")
+const app =express()
+const http=require("http")//coz to build server with socket.io
+const cors=require("cors")
+// ....
+const {Server} =require("socket.io")
+app.use(cors())
+
+const server=http.createServer(app)
+// ....
+const io=new Server(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        methods:["GET","POST"]
+    }
+})
+io.on("connection",(socket)=>{
+console.log(socket.id);
+
+// ....joining room...
+socket.on("join_room",(data)=>{
+    socket.join(data)
+    console.log("join_room",data)
+})
+socket.on("send_msg",(data)=>{
+    console.log(data,typeof data.room)
+    // io.to(data.room).emit("msg",data)
+    socket.broadcast.emit("msg",data)
+    // io.in(data.room).emit("msg",data)
+})
+
+socket.on("disconnect",()=>{
+    console.log("user disconnected",socket.id)
+})
+})
+
+
+
+
+server.listen(3001,()=>{
+    console.log("Server is runing....")
+})
